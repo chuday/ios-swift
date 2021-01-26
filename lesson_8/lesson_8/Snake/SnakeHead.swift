98f57e09e44c93ddea7caf62581f0a29a4b1ca1a
chuday <mikhail_chudaew505@mail.ru>
2021-01-26 08:56:53 +0500

    l-_8

---
+++ lesson_8/lesson_8/Snake/SnakeHead.swift
@@ -0,0 +1,27 @@
+//
+//  SnakeHead.swift
+//  lesson_8
+//
+//  Created by Михаил Чудаев on 25.01.2021.
+//
+
+import UIKit
+
+class SnakeHead: SnakeBodyPart {
+    
+
+    override init(atPoint point: CGPoint) {
+        super.init(atPoint: point)
+        
+        self.physicsBody?.categoryBitMask = CollisionCategories.SnakeHead
+        
+        self.physicsBody?.contactTestBitMask = CollisionCategories.EdgeBody | CollisionCategories.Apple | CollisionCategories.Snake
+
+    }
+    
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
